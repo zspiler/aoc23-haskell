bd34936ca184f7546a040f787166bf09a230b603
Zan Spiler <zan.spiler@celtra.com>
2024-11-03 19:04:09 +0100

    Day 2: improve types

--- 02.hs
+++ 02.hs
@@ -4,15 +4,18 @@ import Data.Maybe (fromJust, isJust)
 import Utils (firstAndLast, readInt, removeSpaces, splitBy, reverseTuple)
 import qualified Data.Map as Map
 
+data Color = Red | Green | Blue deriving (Eq, Ord, Show)
+
+type Game = [Round]
+type Round = [Move]
+type Move = (Int, Color)
+
 main = do
     args <- getArgs
     let inputFile = head args
     contents <- readFile inputFile
     print . solve1 . lines $ contents
     print . solve2 . lines $ contents
-
-availableCubes :: Map.Map String Int
-availableCubes = Map.fromList [("red", 12),("green", 13), ("blue", 14)]
 
 solve1 :: [String] -> Int
 solve1 lines = sum (map fst (filter (\(i, line) -> isGamePossible (parseGame line)) zipped))
@@ -21,29 +24,41 @@ solve1 lines = sum (map fst (filter (\(i, line) -> isGamePossible (parseGame lin
 solve2 :: [String] -> Int
 solve2 lines = sum (map (gamePower . parseGame) lines)
 
-isGamePossible :: [[(Int, String)]] -> Bool
+availableCubes :: Map.Map Color Int
+availableCubes = Map.fromList [(Red, 12), (Green, 13), (Blue, 14)]
+
+isGamePossible :: Game -> Bool
 isGamePossible = all isRoundValid
 
-isRoundValid :: [(Int, String)] -> Bool
+isRoundValid :: Round -> Bool
 isRoundValid = all isMoveValid
 
-isMoveValid :: (Int, String) -> Bool
+isMoveValid :: Move -> Bool
 isMoveValid (amount, color) =  case val of
     Just x -> x >= amount
     Nothing -> False
     where val = Map.lookup color availableCubes
 
-parseGame :: String -> [[(Int, String)]]
+gamePower :: Game -> Int
+gamePower game = product (Map.elems dict)
+    where dict = Map.fromListWith max (map reverseTuple (concat game))
+
+parseGame :: String -> [[Move]]
 parseGame game = map parseRound round
     where round = splitBy ';' (dropTitle game)
           dropTitle s = filter (/= ':') (dropWhile (/= ':') s)
 
-parseRound :: String -> [(Int, String)]
+parseRound :: String -> [Move]
 parseRound round = map parsePair splitStr
     where
-        parsePair [val, color] = (readInt (removeSpaces val), removeSpaces color)
+        parsePair [v, c] = case parseColor(removeSpaces c) of
+            Just color -> (readInt (removeSpaces v), color)
+            Nothing -> error ("Invalid color: " ++ c)
         splitStr = map (splitBy ' ') (splitBy ',' round)
 
-gamePower :: [[(Int, String)]] -> Int
-gamePower game = product (Map.elems dict)
-    where dict = Map.fromListWith max (map reverseTuple (concat game))
+parseColor :: String -> Maybe Color
+parseColor s = case s of 
+    "red" -> Just Red
+    "green" -> Just Green
+    "blue" -> Just Blue
+    _ -> Nothing
