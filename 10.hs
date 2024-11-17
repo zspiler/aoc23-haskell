53e3ecaf915bff347c3d03bf8fa4c41d60c4f0bd
Zan Spiler <zan.spiler@celtra.com>
2024-11-17 15:09:25 +0100

    Day 10 - part 1

---
+++ 10.hs
@@ -0,0 +1,46 @@
+import System.Environment (getArgs)
+import Utils (slice, getAtCoordinate)
+import Data.Maybe (fromJust)
+import Data.List (all, find)
+import qualified Data.Map as Map
+
+main = do
+    args <- getArgs
+    let inputFile = head args
+    contents <- readFile inputFile
+
+    let grid = lines contents
+    let firstMove = (62, 64)
+    let start = findStart grid
+
+    let path = walk grid firstMove [findStart grid]
+    let stepsToFarthestPoint = fromIntegral (length path + 1) / 2
+    print stepsToFarthestPoint
+
+findStart :: [String] -> (Int, Int)
+findStart grid = fromJust $ find (\pos -> getAtCoordinate grid pos  == 'S') coordinates
+    where coordinates = [(x,y) | y <- [0..length grid - 1], x <- [0..length (head grid)-1]]
+
+walk :: [[Char]] -> (Int, Int) -> [(Int, Int)] -> [(Int, Int)]
+walk grid pos path
+    | null nextPipes = path
+    | otherwise = walk grid (head nextPipes) (path ++ [pos])
+    where
+        nextPipes = filter (`notElem` path) (validNeighbourPipes grid pos)
+        element = getAtCoordinate grid pos
+
+validNeighbourPipes :: [String] -> (Int, Int) -> [(Int, Int)]
+validNeighbourPipes grid pos 
+    | element `elem` ['.', 'S'] = []
+    | otherwise = map (applyDirection pos) (fromJust $ Map.lookup element pipeData) 
+    where
+        element = getAtCoordinate grid pos
+        pipeData = Map.fromList [('|', "NS"),('L', "NE"),('J', "NW"),('-', "EW"),('7', "SW"),('F', "SE")]                 
+
+applyDirection :: (Int, Int) -> Char -> (Int, Int)
+applyDirection (x, y) direction
+    | direction == 'N' = (x, y-1)
+    | direction == 'S' = (x, y+1)
+    | direction == 'E' = (x+1, y)
+    | direction == 'W' = (x-1, y)
+    | otherwise = error "🥺"
